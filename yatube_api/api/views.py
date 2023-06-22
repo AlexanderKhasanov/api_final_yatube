55372edde5a008ca71202141739a1b2967c6acc2
Alexander Khasanov <AlexanderKhasanov1205@yandex.ru>
2023-06-22 11:00:34 +0300

    add group

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,20 @@
-# TODO:  Напишите свой вариант
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer
+from posts.models import Post, Comment, Group
+
+
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = LimitOffsetPagination
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,11 +4,47 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+        help_text='Придумайте название группы',
+    )
+    slug = models.SlugField(
+        max_length=20,
+        unique=True,
+        verbose_name='Адрес для страницы группы',
+        help_text=(
+            'Укажите уникальный адрес для страницы задачи. Используйте только '
+            'латиницу, цифры, дефисы и знаки подчёркивания'
+        ),
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+        help_text='Введите описание группы'
+    )
+
+    class Meta:
+        verbose_name = 'group'
+        verbose_name_plural = 'groups'
+
+    def __str__(self) -> str:
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Группа',
+        help_text='Укажите группу, к которой относится пост'
+    )
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True)
 
